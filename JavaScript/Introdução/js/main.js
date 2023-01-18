fa3c86bb8eca7ebcc26fa717e2d187c97bcd7bb6
2005HAK <hebertkubis15@gmail.com>
2023-01-18 12:02:24 -0300

    Introdução JavaScript

---
+++ JavaScript/Introdução/js/main.js
@@ -0,0 +1,17 @@
+var nome = "Hebert A. Kubis";
+var idade = 17;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+
+//alert(nome+" tem "+idade+" anos");
+
+alert(idade+idade2);
+
+console.log(nome);//mostra o nome no console
+console.log(idade);//mostra a idade no console
+console.log(frase.replace("Japão","Brasil"));//muda uma palavra da frase por outra
+alert(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());//deixa todas as letras em maiusculo
+console.log(frase.replace("Japão","Brasil").toLowerCase());//muda uma palavra da frase por outra e deixa tudo em minusculo
+console.log(idade*3);
+console.log(idade+idade2)
